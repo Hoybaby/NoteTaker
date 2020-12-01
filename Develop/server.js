8ec7e22b522d07da5efb2763b12266b54fa4fe53
Hoybaby <mbartek436@gmail.com>
2020-11-30 23:19:22 -0500

    EVERYTHING IS WORKING need small adjustments.

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var path = require('path');
-var apiData = require('./db/db.json')
+// var apiData = require('./db/db.json')
 // Tells node that we are creating an "express" server
 var app = express();
-const fs = require("fs")
-const shortId = require('shortid')
+const fs = require("fs");
+const shortId = require('shortid');
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 3000;
@@ -15,7 +15,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public',)))
+app.use(express.static(path.join(__dirname, 'public',)))
 
 
 // ROUTER
@@ -28,60 +28,70 @@ app.use(express.static(path.join(__dirname,'public',)))
 // var apiRoutes = require("./routes/apiRoute");
 // app.use("/api", apiRoutes);
 
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "public","notes.html"));
-  console.log("Notes by user");
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+  
 });
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname,'public', "index.html"));
-  console.log("Main index");
+  res.sendFile(path.join(__dirname, 'public', "index.html"));
+
 });
 
 
-// app.get("/api/notes", function(req, res) {
 
-//   res.json(apiData);
-
-// });
-
-app.get("/api/notes", function(req, res) {
-  fs.readFile("db/db.json", function(err, data) {
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", function (err, data) {
     if (err) throw err;
     let allNotes = JSON.parse(data);
     return res.json(allNotes);
-} );
+  });
 });
 
-app.post("/api/notes", function(req, res) {
-  fs.readFile("db/db.json", function(err, data) {
+app.post('/api/notes', function (req, res) {
+  fs.readFile("db/db.json", function (err, data) {
     if (err) throw err;
     let allNotes = JSON.parse(data);
     let newNote = {
       title: req.body.title,
       text: req.body.text,
       id: shortId.generate()
+    }
+    
+    allNotes.push(newNote);
+
+    fs.writeFile('db/db.json', JSON.stringify(allNotes, null, 2), (err) => {
+      if (err) throw err;
+      res.send('200') //
+    });
+
+   });
+});
+
+app.delete('/api/notes/:id', function (req, res) {
+  const deletedNotes = req.params.id;
+  fs.readFile("db/db.json", function (err, data) {
+    if (err) throw err;
+    let allNotes = JSON.parse(data);
+  
+  function searchNotes(deletedNotes, allNotes) {
+    for (var i = 0; i < allNotes.length; i++) {
+      if (allNotes[i].id === deletedNotes) {
+        allNotes.splice(i,1);
+      }
+    }
   }
-  console.log('test1');
-  allNotes.push(newNote);
+  searchNotes(deletedNotes, allNotes);
 
-  fs.writeFileSync("db/db.json", JSON.stringify(allNotes, null, 2), (err) => {
-  if(err) throw err;
-  console.log("You got an error")
+  fs.writeFile("db/db.json", JSON.stringify(allNotes, null, 2), (err) => {
+    if (err) throw err;
+    res.send('200') //
+  });
+    
+  });
 });
- 
-} );
-});
 
 
-// fs.writeFileSync(apiData, JSON.stringify(saved))
-
-// let rawdata = fs.readFileSync('./db.json');
-
-// If no matching route is found default to home
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
 
 
 
@@ -89,6 +99,6 @@ app.post("/api/notes", function(req, res) {
 
 // LISTENER
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
